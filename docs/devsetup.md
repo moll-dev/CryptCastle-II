2bf915994d289c20cc50106c2b7e24ad5cc60bb6
Tom <absentisinaventulus@gmail.com>
2014-01-19 23:57:25 -0600

    Updated Test.py and updated dev info

--- docs/devsetup.md
+++ docs/devsetup.md
@@ -10,3 +10,6 @@ http://www.pip-installer.org/en/latest/installing.html
 ```
 2- Follow steps listed here to create a virtual environment 
 http://docs.python-guide.org/en/latest/dev/virtualenvs/
+
+Alternatively this tutorial works pretty well for Windows 
+> http://www.tylerbutler.com/2012/05/how-to-install-python-pip-and-virtualenv-on-windows-with-powershell/
--- src/test.py
+++ src/test.py
@@ -1,6 +1,27 @@
 #BIG OLE TEST 
+import curses 
 
-delim = '> '
-var = raw_input(delim)
+class command(object):
+	string = ""
+	wArray = {}
+	delim  = '>'
+	verb   = ""
+	obj    = ""
 
-print var
+	def read(self):
+		self.string = raw_input(self.delim) #Change to curses some how?
+		self.wArray = self.string.split()
+		
+		if(len(self.wArray) < 2):
+			return 
+		else:
+			self.verb   = self.wArray[0]
+			self.obj    = self.wArray[1]
+
+
+command = command()
+
+command.read()
+
+print "Verb: " + command.verb
+print "Object: " + command.obj

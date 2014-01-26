29414df402ea181728e5f2557ba7a6dc1f3cfb23
Tom <absentisinaventulus@gmail.com>
2014-01-26 00:44:00 -0600

    Setup a TON of stuff, made the importworld.py actually work! and other fun goodies, I highly suggest  checking it out

--- src/importworld.py
+++ src/importworld.py
@@ -1,1 +1,48 @@
-__author__ = 'Michael Snook'
+import xml.dom.minidom
+from map import *
+
+class worldimporter():
+    def __init__(self,engine):
+        self.parent = engine
+
+    def importWorld(self):
+        pass
+
+world1xml = xml.dom.minidom.parse("world1.xml")
+
+
+root = world1xml.getElementsByTagName("World")[0]
+worldattrs = dict(root.attributes.items())
+world = World(worldattrs)
+
+maps = world1xml.getElementsByTagName("Map")
+attrs = dict(maps[0].attributes.items())
+
+
+for x in range(root.getElementsByTagName("Map").length):
+    mapattrs = dict(maps[x].attributes.items())
+    loc = map(int,mapattrs['loc'].split())
+    world.appendMap(mapattrs)
+    for y in range(maps[x].getElementsByTagName("Room").length):
+        roomattrs = dict(maps[x].getElementsByTagName("Room")[y].attributes.items())
+        loc = map(int,mapattrs['loc'].split())
+        world.grid[loc[0]][loc[1]].appendRoom(roomattrs)
+
+print world.grid[5][5].name
+print world.grid[5][5].grid[2][2].exits
+print world.grid[5][5].grid[2][2].canExit("north")
+print world.grid[5][5].grid[2][2].canExit("south")
+print world.grid[5][5].grid[2][2].canExit("east")
+print world.grid[5][5].grid[2][2].canExit("west")
+print world.grid[5][5].grid[2][2].canExit("up")
+print world.grid[5][5].grid[2][2].canExit("down")
+
+
+
+
+
+
+
+
+
+
--- src/map.py
+++ src/map.py
@@ -1,43 +1,68 @@
 import xml.dom.minidom
 world1xml = xml.dom.minidom.parse("world1.xml")
 
-class Map(object):
-    height = 1000
-    width = 1000
-    grid = [[None for x in range(width)]for y in range(height)]
+class World(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, attrs):
+        self.size= map(int,attrs['size'].split())
+        self.name = attrs['name']
+        self.desc = attrs['desc']
+        self.grid = [[None for _ in range(self.size[0])]for _ in range(self.size[1])]
+
+    def appendMap(self, mapattrs):
+        loc = map(int,mapattrs['loc'].split())
+        self.grid[loc[0]][loc[1]] = Map(self,mapattrs)
+
+class Map(object):
+
+    def __init__(self,parent,attrs):
+        self.parent = parent
+        self.loc = map(int,attrs['loc'].split())
+        self.size= map(int,attrs['size'].split())
+        self.name = attrs['name']
+        self.desc = attrs['desc']
+        self.grid = [[None for _ in range(self.size[0])]for _ in range(self.size[1])]
+
+    def appendRoom(self, roomattrs):
+        loc = map(int,roomattrs['loc'].split())
+        self.grid[loc[0]][loc[1]] = Room(self,roomattrs)
 
     def getRoom(self, x,y):
         return self.grid[x][y]
 
 class Room(object):
     contents = [None]
+    directions = {'north':'0','south':'1','east':'2','west':'3','up':'4','down':'5'}
+    def __init__(self, parent,attrs):
+        self.parent = parent
+        self.loc = map(int,attrs['loc'].split())
+        self.name = attrs['name']
+        self.desc = attrs['desc']
+        self.exits = repr(attrs['exits'].split())
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+    def canExit(self,entry):
+        if (entry == "north") and self.exits[0]:
+            return True
+
+        if (entry == "south") and self.exits[1]:
+            return True
+
+        if (entry == "east")  and self.exits[2]:
+            return True
+
+        if (entry == "west")  and self.exits[3]:
+            return True
+
+        if (entry == "up")    and self.exits[4]:
+            return True
+
+        if (entry == "down")  and self.exits[4]:
+            return True
+
+        else:
+            return False
 
 
-castle = Room("Castle","Herpderp")
-
-castle.contents[0] = "Spatula"
-
-a = world1xml.getElementsByTagName("World")[0]
-print a.attributes.items()
 
 
-
-
-
-#Test
-
-map = Map()
-map.grid[10][10] = "Outhouse"
-print map.grid[10][10]
-print map.grid[10][11]
-print castle.contents[0]
-castle.contents.append("Spoon")
-print castle.name
 
